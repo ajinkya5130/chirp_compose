2943ec8a5ae0f89a58b6cf65e2177fa9839d5e5c
AjinkyaKalaskar <ajinkya.kalaskar@movius.ai>
2025-10-01 16:09:38 +0530

    Feat: Add RegisterScreen MVI structure
    - Introduced the basic MVI components for the `RegisterScreen` within the `feature/chat/presentation` module.
    - Added `RegisterScreen.kt` with a root composable, the main UI screen, and a preview.
    - Created `RegisterScreenState.kt` to define the UI state with default parameters.
    - Added `RegisterScreenAction.kt` to define user interactions as a sealed interface.
    - Implemented a basic `RegisterScreenViewModel.kt` with a `MutableStateFlow` for state management and an `onAction` handler.

---
+++ feature/chat/presentation/src/commonMain/kotlin/com/plcoding/chat/presentation/register/RegisterScreenAction.kt
@@ -0,0 +1,9 @@
+/**
+Created by ajinkya on 01/10/25
+ */
+
+package com.plcoding.chat.presentation.register
+
+sealed interface RegisterScreenAction {
+
+}
---
+++ feature/chat/presentation/src/commonMain/kotlin/com/plcoding/chat/presentation/register/RegisterScreenState.kt
@@ -0,0 +1,10 @@
+/**
+Created by ajinkya on 01/10/25
+ */
+
+package com.plcoding.chat.presentation.register
+
+data class RegisterScreenState(
+    val paramOne: String = "default",
+    val paramTwo: List<String> = emptyList(),
+)
